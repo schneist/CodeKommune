b37f4fb01a99ac74b6b26a8ef8a4f6e856934008
Stefan Schneider <schneist@gmail.com>
2016-02-29 15:45:55 +0100

    first test sketch

--- test/JsonTest.scala
+++ test/JsonTest.scala
@@ -1,9 +1,32 @@
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import play.api.mvc.Result
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, WithApplication}
+
+import scala.concurrent.Future
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
   * Created by scsf on 12.02.2016.
   */
-class JsonTest {
+@RunWith(classOf[JUnitRunner])
+class JsonTest extends Specification {
 
 
-  
+  "Application" should {
+
+    "send 404 on a bad request" in new WithApplication{
+      route(FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+    }
+
+    "render the index page" in new WithApplication{
+      val treedata : Future[Result]  = route(FakeRequest(GET, "/treedata.json")).get
+      status(treedata) must equalTo(OK)
+    }
+  }
 
 }
