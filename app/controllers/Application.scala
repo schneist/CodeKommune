3f7e13d9ef24b6317fa75a57293a72baefaf2d45
Stefan Schneider <schneist@gmail.com>
2016-05-11 15:25:25 +0200

    Update Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -24,37 +24,18 @@ class Application(rc: Components) extends Controller {
   }
 
   def treedata = Action.async {
-    val treeFuture:Future[TaskTree] = Future {
-      val treeResult : TaskTree = new TaskTree("root",Seq.empty)
-      // iterate(treeResult)
-      treeResult
-    }
-    treeFuture.map( t => Ok(Json.toJson(t)))
+    iterate(new TaskTree("root", Seq.empty)).map(t => Ok(Json.toJson(t)))
   }
 
-  /*
-    @tailrec
-    private def iterate(tree : Future[TaskTree]):Future[TaskTree]= {
-      tree.flatMap(t => getLeaves(t, Seq.empty))
-
-       .map(p => (p, taskRepo.childTaskFinder.getChildren(p.name))).
-        map(p => p._2.map( s => p._1.children.++(s).size))
-      .exists(fint => fint  >=0).)
-      iterate(tree)
-
-    }
-
-    private def getLeaves(tree: TaskTree,leaves : Seq[TaskTree]): Future[Seq[TaskTree]] ={
-      tree.children.size match{
-        case 0 => Future{leaves.+:(tree)}
-        case _ => tree.children.flatMap( l =>  getLeaves(l,leaves).c)
+  private def iterate(tree: TaskTree): Future[TaskTree] = {
+    taskRepo.childTaskFinder.getChildren(tree.name).map(l => {
+      tree.children.size match {
+        case 0 => new TaskTree(tree.name, l)
+        case _ => return iterate(new TaskTree(tree.name, l))
       }
     }
-  */
-
-
-
-
+    )
+  }
 
 }
 
